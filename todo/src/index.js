7fb8b109692132a689dcf913fbae8198403b6fc7
Theodore Wiersema <theo.ew@gmail.com>
2020-09-27 14:42:02 -0500

    Moved tasks to reside in App state

--- todo/src/index.js
+++ todo/src/index.js
@@ -1,76 +1,139 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import * as Realm from 'realm-web';
-
-const bson = require('bson');
-
+import bson from 'bson';
 
 // Connection
 const app = new Realm.App({ id: "todo-app-mnupq", timeout: 10000 });
 const mongoCol = app.services.mongodb('mongodb-atlas').db('data').collection('tasks');
 
+/*
+    MAIN WRAPPER
+*/
 class App extends React.Component {
     constructor(props) {
         super();
+        this.newTaskTitle = React.createRef();
         this.state = {
-            user:app.currentUser,
-            toggle:false,
+            tasks:[],
+            user:app.currentUser
         }
     }
-    render() {
-        const addTask = async () => {
-            const newTask = {
-                title: 'SOS',
-                status: true
-            }
+    componentDidMount() {
+        // anonymous function to retrieve tasks from server when page loads
+        (async () => {
             try {
-                await mongoCol.insertOne(newTask);
-                this.setState({toggle:!this.state.toggle})
+                const tasks = await mongoCol.find({status:true}); // find non-deleted tasks
+                this.setState({tasks:tasks})
+            } catch {
+                return 'Failed to retrieve tasks';
+            }
+        })();
+    }
+    render() {
+        /*
+            ADD NEW TASK
+        */
+        const addTask = async (event) => {
+            // prevent page from refreshing
+            event.preventDefault(); 
+            // get value of input field
+            const taskTitle = this.newTaskTitle.current.value; 
+            // don't continue if empty
+            if (taskTitle==='') return false; 
+            // server operations
+            try {
+                // add new tasks to server 
+                const newID = await mongoCol.insertOne({
+                    title:taskTitle,
+                    status:true
+                });
+                // update state with new task (for real time updates on page)
+                this.setState((state) => {
+                    state.tasks.push({
+                        _id: new bson.ObjectId(newID.insertedId.id), // extract actual ID
+                        title: taskTitle,
+                        status: true
+                    });
+                    return {tasks:state.tasks}
+                });
             } catch {
                 console.log('Adding task failed!');
             }
+
+            // clear input fields in form
+            document.getElementById('newTaskEntry').reset();
         }
+        /*
+            LOG OUT
+        */
         const logout = async () => {
             try {
-                await app.currentUser.logOut().then(this.setState({user:''}));
+                await app.currentUser.logOut().then(this.setState({
+                    user:''
+                }));
             } catch {
                 console.log('Unable to Log out');
             }
-            
         }
+        /*
+            DELETE SINGLE TASK
+        */  
         const deleteTask = async (id) => {
             try {
-                const o_id = new bson.ObjectId(id);
-                const found = await mongoCol.deleteOne({_id:o_id});
-                console.log(found);
+                // get index of task we're removing
+                const index = this.state.tasks.findIndex((el) => el._id.toString() === id);
+                // update state
+                this.setState((state) => {
+                    state.tasks.splice(index,1);
+                    return {tasks:state.tasks}
+                });
+                // remove tasks on server
+                await mongoCol.deleteOne({
+                    _id: new bson.ObjectId(id)
+                });
             } catch {
                 console.log('Unable to delete Task.')
             }
         }
+        /*
+            DELETE ALL TASKS 
+        */
         const deleteAllTasks = async () => {
             try {
-                const allTasks = {
-                    status: true
-                }
-                await mongoCol.deleteMany(allTasks);
+                await mongoCol.deleteMany({status:true});
             } catch {
                 console.log('Unable to delete all tasks!')
             }
         }
+        /*
+            LOG IN
+        */  
         const login = async () => {
             const user = await app.logIn(Realm.Credentials.anonymous());
             this.setState({user:user});
         };
+        /*
+            IF LOGGED IN
+        */  
         if (this.state.user) {
             return (
                 <div>
-                    <NewTaskButton addTask={addTask} />
+                    <NewTaskEntry 
+                        newTaskTitle={this.newTaskTitle} 
+                        addTask={addTask} 
+                    />
+                    <TaskList 
+                        tasks={this.state.tasks}
+                        deleteTask={deleteTask}
+                    />    
                     <LogOutButton logout={logout} />
-                    <DeleteAllButton deleteAllTasks={deleteAllTasks} />
-                    <TaskList deleteTask={deleteTask} toggle={this.state.toggle} />
-                    
+                    <DeleteAllButton deleteAllTasks={deleteAllTasks} />  
                 </div>  
             )
+        /*
+            IF LOGGED OUT
+        */
         } else {
             return (
                 <LogInButton login={login} />
@@ -80,44 +143,8 @@ class App extends React.Component {
 }
 
 class TaskList extends React.Component {
-    constructor(props) {
-        super();
-        this.state = {
-            tasks:[],
-            toggle:props.toggle
-        };
-        
-    }
-    componentDidMount() {
-        const getTasks = async () => {
-            try {
-                const tasks = await mongoCol.find();
-                this.setState({tasks:tasks})
-            } catch {
-                return 'Failed to retrieve tasks';
-            }
-        }
-        getTasks();
-        
-    }  
-    componentDidUpdate(prevProps) {
-        if (this.props.toggle !== prevProps.toggle) {
-            this.setState({
-                toggle:this.props.toggle
-            })
-            const getTasks = async () => {
-                try {
-                    const tasks = await mongoCol.find();
-                    this.setState({tasks:tasks})
-                } catch {
-                    return 'Failed to retrieve tasks';
-                }
-            }
-            getTasks();
-        }
-    }
     render() {    
-        let taskList = this.state.tasks.map((task,index) => {
+        let taskList = this.props.tasks.map((task,index) => {
             return(
                 <Task 
                     key={task._id} 
@@ -126,9 +153,10 @@ class TaskList extends React.Component {
                     deleteTask={this.props.deleteTask}
                 />
             );
-        }).reverse();
+        }).reverse(); // puts most recent task on top
+        // handle "loading"
         if (taskList.length === 0) {
-            return <p>Loading...</p>
+            return <p>No tasks</p>
         } else {
             return <ul>{taskList}</ul>
         }
@@ -151,13 +179,19 @@ class Task extends React.Component {
 
 class DeleteTaskButton extends React.Component {
     render() {
-        return <button onClick={() => this.props.deleteTask(this.props.id)}>Delete Task</button>
+        return <button onClick={() => this.props.deleteTask(this.props.id)}>Delete</button>
     }
 }
 
-class NewTaskButton extends React.Component {
+class NewTaskEntry extends React.Component {
     render() { 
-        return <button onClick={() => this.props.addTask()}>Add Task</button>
+        return (
+            <form id="newTaskEntry">
+                <input type="text" ref={this.props.newTaskTitle} />
+
+                <button onClick={(e) => this.props.addTask(e)}>Add Task</button>
+            </form>       
+        );
     }
 }
 class LogOutButton extends React.Component {
@@ -177,6 +211,5 @@ class DeleteAllButton extends React.Component {
         return <button onClick={() => this.props.deleteAllTasks()}>Delete All</button>
     }
 }
-
 
 ReactDOM.render(<App />,document.getElementById('root'));
